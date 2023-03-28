4a0a040839af85005801011a07b81fda28b603ee
Robert <mirob1179@gmail.com>
2023-03-28 21:27:51 +0300

    Corrected small bugs and added loggers

--- src/main/java/me/learnspring/restservices/controllers/PaymentsControllerForRestConsumption.java
+++ src/main/java/me/learnspring/restservices/controllers/PaymentsControllerForRestConsumption.java
@@ -8,16 +8,23 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentsControllerForRestConsumption {
 
-    @PostMapping
+    private final Logger logger = Logger.getLogger(PaymentsControllerForRestConsumption.class.getName());
+
+    @PostMapping("/payments")
     public ResponseEntity<PaymentDetails> createPayment(
             @RequestHeader String requestId,
             @RequestBody PaymentDetails paymentDetails
     ) {
+        logger.info("Received request with ID " +
+                requestId + " ;Payment Amount: " + paymentDetails.getAmount());
+
         paymentDetails.setId(UUID.randomUUID().toString());
+        paymentDetails.setAmount(paymentDetails.getAmount() + 100d);
 
         return ResponseEntity
                 .accepted()
